6f16ba0a43af3ad12cdfb056c62693cb9e1d9d99
gauffret <gurvan.auffret@gmx.fr>
2019-10-04 17:30:54 +0200

    Update algo

--- src/modules/n-puzzle/n-puzzle.service.ts
+++ src/modules/n-puzzle/n-puzzle.service.ts
@@ -1,8 +1,10 @@
-import { Component, BadRequestException } from '@nestjs/common';
+import { Component, BadRequestException, flatten } from '@nestjs/common';
 import { ResolvePuzzleDto } from './dto/resolve-puzzle.dto';
 import * as _ from 'lodash'
 import { NPuzzle } from 'n-puzzle-entity/dist/server/n-puzzle/n-puzzle.entity'
 import { NPuzzleAlgo } from 'n-puzzle-entity/dist/server/n-puzzle/enums/n-puzzle-algo.enum';
+import { getCustomRepository } from 'typeorm';
+import { assertExpressionStatement, TSExpressionWithTypeArguments } from 'babel-types';
 
 @Component()
 export class NPuzzleService {
@@ -13,7 +15,7 @@ export class NPuzzleService {
    * 
    * @param dto 
    */
-  async resolvePuzzle(puzzle: string, type: NPuzzleAlgo): Promise<NPuzzle> {
+  async resolvePuzzle(puzzle: string, type: NPuzzleAlgo){ //: Promise<NPuzzle> {
     let fileChecker: FileChecker = this.checkFile(puzzle)
     let finalBoard: number[][] = this.generateFinalBoard(fileChecker.size);
     let nPuzzle = new NPuzzle()
@@ -21,8 +23,10 @@ export class NPuzzleService {
     nPuzzle.origin = fileChecker.board
     nPuzzle.type = type
     console.log('Goal is : \n', this.boardToString(nPuzzle.final))
-    nPuzzle = this.solve(nPuzzle)
-    return nPuzzle
+    //nPuzzle = this.solve(nPuzzle)
+    //return nPuzzle
+    let search = new SearchUsingAStar(new State(_.flatten(nPuzzle.origin)), new State(_.flatten(nPuzzle.final)))
+    return await search.search() 
   }
 
   /**
@@ -283,3 +287,371 @@ enum MoveDirection {
 	BOTTOM = 'BOTTOM',
 	LEFT = 'LEFT',
 }
+
+export class State {
+  private _emptyTileIndex: number
+  private _arr: number[]
+  private _numRowsOrCols: number
+  private _neighbours: Map<number, number[]>  = new Map<number, number[]>();
+  private _swip: number
+
+  public set arr(arr: number[]) { this._arr = arr }
+  public get arr(): number[] { return this._arr }
+
+  public get swip(): number { return this._swip }
+
+  public set numRowsOrCols(numRowsOrCols: number) { this._numRowsOrCols = numRowsOrCols }
+  public get numRowsOrCols(): number { return this._numRowsOrCols }
+
+  public set emptyTileIndex(emptyTileIndex: number) { this._emptyTileIndex = emptyTileIndex }
+  public get emptyTileIndex(): number { return this._emptyTileIndex }
+  
+  constructor(args: any) {
+    if (typeof(args) === 'number') {
+      this.numRowsOrCols = args
+      this.arr = new Array(this.numRowsOrCols * this.numRowsOrCols)
+                          .fill(0)
+                          .map((value: number, index: number) => {
+                            if (index === 0) {
+                              this.emptyTileIndex = index
+                            }
+                            return index
+                          })
+    } else if (Array.isArray(args)) {
+      console.log('Construct from array')
+      this.numRowsOrCols = Math.sqrt(args.length);
+      this.arr = new Array(this.numRowsOrCols * this.numRowsOrCols)
+                          .fill(0)
+                          .map((value: number, index: number) => {
+                            if (args[index] === 0) {
+                              this.emptyTileIndex = index
+                            }
+                            return args[index]
+                          })
+    } else {
+      this.numRowsOrCols = args.numRowsOrCols;
+      this.emptyTileIndex = args._emptyTileIndex;
+      this.arr = new Array(this.numRowsOrCols * this.numRowsOrCols)
+                          .fill(0)
+                          .map((value: number, index: number) => {
+                            if (args.arr[index] === 0) {
+                              this.emptyTileIndex = index
+                            }
+                            return args.arr[index]
+                          })
+    }
+    this.createGraphForNPuzzle()
+  }
+
+  public equals(tileA: State, tileB: State): boolean {
+    return tileA.arr.toString() === tileB.arr.toString()
+  }
+
+  public findEmptyTileIndex(): number {
+    return this.arr.findIndex(value => value === 0)
+  }
+
+  public randomize(): void {
+    let randomizedArray: number[] = []
+    let currentArray: number[] = _.cloneDeep(this.arr)
+    while(currentArray.length) {
+      let index = Math.floor(Math.random() * currentArray.length)
+      randomizedArray.push(currentArray.splice(index, 1).shift())
+    }
+    this.arr = randomizedArray
+  }
+
+  // TODO: Fix swap
+  public swapWithEmpty(index: number): void {
+    let tmp: number = this.arr[index];
+    this.arr[index] = this.arr[this.emptyTileIndex];
+    this.arr[this.emptyTileIndex] = tmp;
+    this.emptyTileIndex = index;
+    this._swip = index
+  }
+
+  public getManhattanCost(): number {
+    let cost: number = 0
+    for (let i: number = 0 ; i < this.arr.length ; ++i) {
+      let v: number = this.arr[i]
+      if (v == 0) continue
+
+      // actual index of v should be v-1
+      v = v - 1;
+      let gx: number = v % this.numRowsOrCols
+      let gy: number = v / this.numRowsOrCols
+
+      let x: number = i % this.numRowsOrCols;
+      let y: number = i / this.numRowsOrCols;
+
+      let mancost: number = Math.abs(x - gx) +  Math.abs(y - gy);
+      cost += mancost;
+    }
+    return cost;
+  }
+
+  public getNeighbours(id: number) : number[] {
+    return this._neighbours.get(id);
+  }
+
+  public createGraphForNPuzzle(): void {
+    let numRowsOrCols: number = this.numRowsOrCols
+    let arr: number[] = this.arr
+
+
+    //console.log(this.createGridToPrint())
+
+    for(let i = 0; i < numRowsOrCols; i++) {
+      for(let j = 0; j < numRowsOrCols; j++) {
+        let index: number = i * numRowsOrCols + j
+        //console.log(`current index : ${index}, value ${arr[index]}`)
+        let li: number[] = []
+        if (i - 1 >= 0) {
+          li.push(arr[(i - 1)* numRowsOrCols + j])
+        }
+        if (i + 1 < numRowsOrCols) {
+          li.push(arr[(i + 1)*numRowsOrCols +j]);
+        } 
+        if (j - 1 >= 0) {
+          li.push(arr[i*numRowsOrCols + (j - 1)]);
+        } 
+        if (j + 1 < numRowsOrCols) {
+          li.push(arr[i* numRowsOrCols + (j + 1)]);
+        }
+        //console.log(`current index : ${index}, value ${arr[index]}, li: [${li.join(', ')}]`)
+        this._neighbours.set(index, li)
+      }
+    }
+
+  }
+
+  public createGridToPrint() {
+    let arr: number[][] = _.chunk(this.arr, this.numRowsOrCols)
+
+    let numLen = this.numRowsOrCols.toString().length
+    let b: string = ''
+    for (let r of arr) {
+      let s: string = (b.length ? '\n' : '')+'\t'
+      for (let n of r) {
+        let ns = n.toString()
+        while (ns.length < numLen) {
+          ns = ' '+ns
+        }
+        s += ns+' '
+      }
+      b += s
+    }
+    return b
+  }
+}
+
+// Neighbours class.
+/// this is the class that creates and holds the graph relationship for the 8 puzzle and the empty slot.
+/// note that for this simple configuration the 8 puzzle graph is hardcoded.
+export class Neighbours {
+  private _edges: Map<number, number[]>  = new Map<number, number[]>();
+  private _instance: Neighbours = null;
+
+  constructor() {}
+
+  public set instance(instance: Neighbours) { this._instance = instance }
+  public get instance(): Neighbours { return this._instance || new Neighbours() }
+
+  public getNeighbours(id: number) : number[] {
+    return this._edges.get(id);
+  }
+
+  public createGraphForNPuzzle(rowsOrCols: number): void {
+    for(let i = 0; i < rowsOrCols; i++) {
+      for(let j = 0; j < rowsOrCols; j++) {
+        let index: number = i * rowsOrCols + j
+        let li: number[] = []
+        if (i - 1 >= 0) {
+          li.push((i - 1)* rowsOrCols + j)
+        }
+        if (i + 1 < rowsOrCols) {
+          li.push((i + 1)*rowsOrCols +j);
+        } 
+        if (j - 1 >= 0) {
+          li.push(i*rowsOrCols + j - 1);
+        } 
+        if (j + 1 < rowsOrCols) {
+          li.push(i* rowsOrCols + j + 1);
+        } 
+        console.log(index, li)
+        this._edges.set(index, li)
+        console.log(this._edges.keys())
+      }
+    }
+  }
+}
+
+// class Node
+/// <summary>
+/// A class to hold the State of the 8 puzzle. This is used to create the solution.
+/// Node not only holds the state of the 8 puzzle but also the cost associated with the specific state.
+/// </summary>
+export class Node {
+  private _state: State
+  private _parent: Node
+  private _cost: number
+  private _depth: number
+
+  public set state (state: State) { this._state = state }
+  public get state (): State { return this._state }
+
+  public set parent (parent: Node) { this._parent = parent }
+  public get parent (): Node { return this._parent }
+
+  public set cost (cost: number) { this._cost = cost }
+  public get cost (): number { return this._cost }
+
+  public set depth (depth: number) { this._depth = depth }
+  public get depth (): number { return this._depth }
+
+
+  constructor(state: State, depth: number = 0, parent: Node = null) {
+    this.state = state;
+    this.cost = /*_state.GethammingCost() + */this.state.getManhattanCost() + depth;
+    this.parent = parent;
+    this.depth = depth;
+  }
+
+  // comparison operator based on the cost of the nodes. 
+  // This can be used for sorting nodes based on the cost of 
+  // the state that it holds.
+  public isSuperior(n1: Node, n2: Node): boolean {
+    return n1.cost > n2.cost;
+  }
+
+  public isInferior(n1: Node, n2: Node): boolean {
+    return n1.cost < n2.cost;
+  }
+
+  // print the node into the Debug log.
+  public print(lineNum: number): void {
+    let str: string = ''
+
+    str += `${lineNum} -`
+    str += `Node { `
+    for (let elem of this.state.arr) {
+      str += `${elem}`
+    }
+    str += ` | D: ${this.depth}, MD: ${this.cost} }`
+    console.log(str)
+    //Debug.Log(str.ToString());
+  }
+}
+
+// A rudimentary PriorityQueue implementation.
+// it does not cater for performance or efficiency.
+export class PriorityQueue {
+  // The items and priorities.
+  private _nodes: Node[] = []
+
+  // Return the number of items in the queue.
+  public count():number {
+    return this._nodes.length;
+  }
+
+  // Add an item to the queue.
+  public add(n: Node): void {
+    this._nodes.push(n);
+  }
+
+  // Get the item with the highest priority (in our case the lowest cost)
+  public getAndRemoveTop(): Node {
+    // Find the hightest priority.
+    let bestIndex: number = 0;
+    let bestPriority: number = this._nodes[0].cost;
+    for (let i = 1; i < this.count(); i++) {
+      if (bestPriority > this._nodes[i].cost) {
+        bestPriority = this._nodes[i].cost;
+        bestIndex = i;
+      }
+    }
+
+    return this._nodes.splice(bestIndex, 1).shift()
+  }
+}
+
+// The A Star search alogorithm implementation for solving 8 puzzle problem.
+// This is implemented as a coroutine for Unity. 
+//IEnumerator
+export class SearchUsingAStar {
+  root: Node
+  start: State
+  goal: State
+  
+  constructor(start: State, goal: State) {
+    this.start = start
+    this.goal = goal
+    this.root = new Node(start, 0, null);
+  }
+
+  isClosed(state: State, closedlist: Node[]){
+    return closedlist.map(n => n.state.arr.toString()).indexOf(state.arr.toString()) !== -1
+  }
+
+  async search() {
+    let openlist: PriorityQueue = new PriorityQueue();
+    let closedlist: Node[] = []
+    openlist.add(this.root);
+    closedlist.push(this.root);
+  
+    let solved: boolean = false
+    let solution: Node[] = []
+  
+    console.log('OPEN LIST RESTANTE : ', openlist.count())
+    while (openlist.count() > 0 && !solved) {
+      let current: Node = openlist.getAndRemoveTop();
+      console.log('PARENT : \n', current.parent ? current.parent.state.createGridToPrint() :'\n' ,'\nCURRENT ['+current.state.swip+'] : \n', current.state.createGridToPrint(),'\nGOAL :\n',this.goal.createGridToPrint())
+  
+      if (this.goal.equals(current.state, this.goal)) {
+        // fil the solution.
+        let s: Node = current;
+        do {
+          solution.push(s);
+          s = s.parent;
+        } while (s != null);
+
+        console.log(`Solution found..\nTotal moves needed : ${solution.length}`);
+  
+        solved = true;
+        break;
+      }
+  
+      let zero: number = current.state.findEmptyTileIndex();
+      let neighbours: number[] = current.state.getNeighbours(zero);
+      console.log('Voisins: ', neighbours)
+  
+      for (let next of neighbours) {
+          let state: State = new State(current.state);
+          console.log(`Swip tile ${next}`)
+          state.swapWithEmpty(next);
+          //SwapTiles(next, state, false);
+          console.log('Liste fermees: ', closedlist.length)
+          if (!this.isClosed(state, closedlist)) {
+            console.log('Add to openList')
+              let n: Node = new Node(state, current.depth + 1);
+              n.parent = current;
+              openlist.add(n);
+              closedlist.push(n);
+              //n.Print(++s_lineNum);
+          } else {
+            console.log('Already explore')
+          }
+          await new Promise(
+            (resolve, reject) => {
+            setTimeout(() => {
+              resolve()
+            }, 1000)
+          })
+      }
+    }
+  
+    return solution
+  }
+  
+}
+
