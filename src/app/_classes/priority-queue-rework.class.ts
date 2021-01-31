7d4eff200bacc3fa868ac2abb39a94cda1bd9615
Akraysno <gurvan.auffret@gmx.fr>
2021-01-31 15:50:25 +0100

    Remove useless functions in PriorityQueue & fix getAndRemoveTop

--- src/app/_classes/priority-queue-rework.class.ts
+++ src/app/_classes/priority-queue-rework.class.ts
@@ -1,57 +1,51 @@
 import { State } from "./state-rework.class";
 
 export class PriorityQueue {
-    // The items and priorities.
     private _nodes: Map<number, State[]> = new Map()
   
-    // Return the number of items in the queue.
+    /**
+     * Return the number of items in queue.
+     */
     public count(): number {
       let count = 0
+      //console.log(this._nodes)
       this._nodes.forEach((nodes, index) => count += nodes.length);
       return count
     }
   
+    /**
+     * Get maximum cost store in queue
+     */
     public maxCost(): number {
-      return Array.from(this._nodes.keys()).pop()
+      return Math.max(...Array.from(this._nodes.keys()))
     }
   
+    /**
+     * Get minimum cost store in queue
+     */
     public minCost(): number {
-      return Array.from(this._nodes.keys()).shift()
+      return Math.min(...Array.from(this._nodes.keys()))
     }
-  
-    // Add an item to the queue.
-    // Old, to delete
-    public add(n: State): void {
-      let key = n.cost
-      let pack = this._nodes.get(key)
-      if (!pack) {
-        pack = []
-      }
-      pack.push(n)
-      this._nodes.set(key, pack)
-    }
-    // Add an item to the queue.
-    public addItem(n: State, cost: number): void {
+
+    /**
+     * Add an item to the queue.
+     * 
+     * @param state 
+     * @param cost 
+     */
+    public addItem(state: State, cost: number): void {
       let key = cost
       let pack = this._nodes.get(key)
       if (!pack) {
         pack = []
       }
-      pack.push(n)
+      pack.push(state)
       this._nodes.set(key, pack)
     }
   
-    // Add an item to the queue.
-    public addToQueue(n: State, cost: number): void {
-      let pack = this._nodes.get(cost)
-      if (!pack) {
-        pack = []
-      }
-      pack.push(n)
-      this._nodes.set(cost, pack)
-    }
-  
-    // Get the item with the highest priority (in our case the lowest cost)
+    /**
+     * Get the item with the highest priority (in our case the lowest cost)
+     */ 
     public getAndRemoveTop(): State {
       let key = this.minCost()
       let pack = this._nodes.get(key)
@@ -61,18 +55,5 @@ export class PriorityQueue {
       }
   
       return node
-    }
-  
-    public isEmpty(): boolean {
-      let keys = this._nodes.keys()
-      for (let k of keys) {
-        let n = this._nodes.get(k)
-        if (!n || !n.length) {
-          this._nodes.delete(k)
-        } else {
-          return false
-        }
-      }
-      return true
     }
   }
