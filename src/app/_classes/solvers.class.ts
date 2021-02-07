046ba5484283f9b6a8e6feaf4bb2031408ddfe33
Akraysno <gurvan.auffret@gmx.fr>
2021-02-07 21:30:26 +0100

    Remove useless solver

--- src/app/_classes/solvers.class.ts
+++ src/app/_classes/solvers.class.ts
@@ -14,79 +14,8 @@ export class Solvers {
     static solvers = {
         [NPuzzleAlgo.ASTAR]: Solvers.aStar,
         [NPuzzleAlgo.BEST_FIRST]: Solvers.bestFirst,
-        [NPuzzleAlgo.SLOW_ASTAR]: null,
         [NPuzzleAlgo.WEIGHTED_ASTAR]: Solvers.aStar,
     }
-    /*
-        static _aStarStandard(start: State, heuristic: dist_heuristic): [State, number, number] {
-            const opened = new PriorityQueue<State>();
-            const closed = new Map<string, number>();
-            let nb_tries = 0;
-            opened.insert(start, start.totalDist(heuristic));
-            while (!opened.is_empty()) {
-                nb_tries += 1;
-                const state_e = opened.min();
-                if (state_e.get_h(heuristic) === 0) {
-                    return [state_e, nb_tries, opened.length + closed.size];
-                }
-                closed.set(state_e.hash(), state_e.g);
-                for (const state of state_e.expand()) {
-                    const similar_state: State | undefined = opened.get(state);
-                    if (similar_state) {
-                        if (state.g < similar_state.g) {
-                            opened.insert(state, state.totalDist(heuristic));
-                        }
-                        continue;
-                    }
-                    const closed_dist = closed.get(state.hash());
-                    if (typeof closed_dist !== 'undefined') {
-                        if (state.g < closed_dist) {
-                            opened.insert(state, state.totalDist(heuristic));
-                            closed.delete(state.hash());
-                        }
-                        continue;
-                    }
-                    opened.insert(state, state.totalDist(heuristic));
-                }
-            }
-            throw 'impossible';
-        }
-    
-        static aStarStandard(start: State, heuristic: DistHeuristic) {
-            const opened = new PriorityQueue();
-            const closed: Set<String> = new Set()
-            let nb_tries = 0;
-            opened.add(start, start.totalDist(heuristic));
-            while (!opened.is_empty()) {
-                nb_tries += 1;
-                const state_e = opened.min();
-                if (state_e.get_h(heuristic) === 0) {
-                    return [state_e, nb_tries, opened.length + closed.size];
-                }
-                closed.set(state_e.hash(), state_e.g);
-                for (const state of state_e.expand()) {
-                    const similar_state: State | undefined = opened.get(state);
-                    if (similar_state) {
-                        if (state.g < similar_state.g) {
-                            opened.insert(state, state.totalDist(heuristic));
-                        }
-                        continue;
-                    }
-                    const closed_dist = closed.get(state.hash());
-                    if (typeof closed_dist !== 'undefined') {
-                        if (state.g < closed_dist) {
-                            opened.insert(state, state.totalDist(heuristic));
-                            closed.delete(state.hash());
-                        }
-                        continue;
-                    }
-                    opened.insert(state, state.totalDist(heuristic));
-                }
-            }
-            throw 'impossible';
-        }
-        */
-
 
     static bestFirst(start: State, heuristic: DistHeuristic, openedList: OpenedList, closedList: ClosedList): State {
         openedList.addItem(start, start.getDist(heuristic));
